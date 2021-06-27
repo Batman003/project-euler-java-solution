fdc94f836c54daec2a40d2cc3c1e81d8b65ff450
shadab003 <shadabchaudhary7499@gmail.com>
2021-06-28 00:36:13 +0530

    Problem 20 added!

---
+++ project-euler/src/ques11to20/Ques20.java
@@ -0,0 +1,40 @@
+package ques11to20;
+
+import java.math.BigInteger;
+
+
+//QUESTION 20 : 
+//	
+//	n! means n × (n − 1) × ... × 3 × 2 × 1
+//
+//	For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+//	and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+//
+//	Find the sum of the digits in the number 100!
+
+public class Ques20 {
+
+	
+	public static BigInteger getFactorial(int num) {
+		BigInteger getFactorial = BigInteger.ONE;
+		for(int i = 0;i<num;i++) {
+			getFactorial = getFactorial.multiply(BigInteger.valueOf(num-i));
+			//System.out.println(i+ "getFactorial:: "+getFactorial);
+		}
+		
+		BigInteger result = BigInteger.ZERO;
+		
+		while(getFactorial != BigInteger.ZERO) {
+			BigInteger temp = getFactorial.mod(BigInteger.TEN);
+			getFactorial = getFactorial.divide(BigInteger.TEN);
+			result = result.add(temp);
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		BigInteger result = getFactorial(100);
+		System.out.println(result);
+	}
+}
